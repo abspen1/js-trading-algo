d8b8efb627a080e8fc6504c839c1287c97583876
abspen1 <abspencer2097@gmail.com>
2020-09-15 18:20:48 -0700

    update for pages

--- algo.js
+++ algo.js
@@ -1,17 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const API_KEY = process.env.APCA_API_KEY_ID;
-const API_SECRET = process.env.APCA_API_SECRET_KEY;
-const PAPER = true
-
-class FalconOne {
-    constructor(API_KEY, API_SECRET, PAPER) {
-        this.Alpaca = require("@alpacahq/alpaca-trade-api");
-        this.alpaca = new this.Alpaca({
-        keyId: API_KEY,
-        secretKey: API_SECRET,
-        paper: PAPER,
+class algo {
+    constructor(API_KEY, API_SECRET) {
+        this.alpaca = new AlpacaCORS({
+            keyId: API_KEY,
+            secretKey: API_SECRET,
+            baseUrl: "https://paper-api.alpaca.markets",
         });
         // Example portfolio holdings with their respective weights
         this.stock1 = "TLT"; // BONDS ETF
@@ -20,23 +12,24 @@ class FalconOne {
         this.weight2 = 0.10;
         this.stock3 = "QQQ"; // NASDAQ ETF
         this.weight3 = 0.25;
-        this.stock4 = "DIA"; // DOW INDUSTRIAL ETF
+        this.stock4 = "DOW"; // DOW INDUSTRIAL ETF
         this.weight4 = 0.1;
         this.stock5 = "SPY"; // S&P 500 ETF
         this.weight5 = 0.25;
     }
 
     async run() {
-        let i = 0
-        let asset
-        let weight
-        let currentPrice
-        let account = await this.alpaca.getAccount()
+        await this.updatePositions();
+        let i = 0;
+        let asset;
+        let weight;
+        let currentPrice;
+        let account = await this.alpaca.getAccount();
         // Get our account value
         const portfolioValue = account.portfolio_value;
 
-        const portfolio = await this.alpaca.getPositions()
-        do{
+        const portfolio = await this.alpaca.getPositions();
+        do {
             i += 1;
             switch (i) {
                 case 1:
@@ -60,151 +53,197 @@ class FalconOne {
                     weight = this.weight5;
                     break;
             }
-            console.log(`\nRebalancing our holdings in ${asset}...\n`);
+
 
             let qty = await this.get_asset_qty(asset, portfolio);
-            console.log(`Holding ${qty} shares of ${asset}`);
-            if ( qty === 0 ) {
-                currentPrice = await this.get_curr(asset)
+            // writeToEventLog(`Holding ${qty} shares of ${asset}`);
+            if (qty === 0) {
+                currentPrice = await this.get_curr(asset);
             } else {
                 currentPrice = await this.get_asset_curr(asset, portfolio);
             }
-            console.log(`${asset}'s current price: ${currentPrice}`);
-            qty = await this.get_qty(currentPrice, qty, portfolioValue, weight)
-            await this.order_target(asset, weight, qty)
-                 
-        } while (i < 5)
-        console.log("\nFinished running Falcon One\n")
+            qty = await this.get_qty(currentPrice, qty, portfolioValue, weight);
+            await this.order_target(asset, weight, qty);
+            writeToEventLog(`${asset}'s current price: ${currentPrice}`);
+            writeToEventLog(`Rebalancing our holdings in ${asset}...\n`);
+            writeToEventLog("\n===================");
+        } while (i < 5);
         account = await this.alpaca.getAccount();
-        console.log(`Portfolio's current value: ${account.portfolio_value}`)
-        console.log(`Account Cash: ${account.cash}`);
-        console.log(`Short Market Value: ${account.short_market_value}\n`);
+        writeToEventLog(`\nShort Market Value: ${account.short_market_value}\n`);
+        writeToEventLog(`Account Cash: ${account.cash}`);
+        writeToEventLog(`Portfolio's current value: ${account.portfolio_value}`);
+        writeToEventLog("Finished running algo\n");
     } // Finished run
-
 
     // Get the quantity of holdings in specific stock
     async get_asset_qty(asset, portfolio) {
-        let qty = 0
-        let holding = false
+        let qty = 0;
         // Print the quantity of shares for each position.
         portfolio.forEach(function (position) {
             if (position.symbol === asset) {
-                // console.log(`${position.qty} shares of ${position.symbol}`);
-                qty = position.qty
-                holding = true
+                // writeToEventLog(`${position.qty} shares of ${position.symbol}`);
+                qty = position.qty;
             }
         });
-        if ( !holding ) {
-            console.log(`Not currently holding ${asset}`);
-            return qty
-        }
-        return qty
+        return qty;
     }
-
 
     // Get current price of stock we dont hold
     async get_curr(asset) {
         var bars;
-        await this.alpaca
-          .getBars("minute", asset, { limit: 20 })
-          .then((resp) => {
-            bars = resp[asset];
-          })
-          .catch((err) => {
-            console.log(err.error);
-          });
-        var currPrice = bars[bars.length - 1].closePrice;
-        return currPrice
+        var currPrice = new Promise(async (resolve, reject) => {
+            await this.alpaca
+                .getBars("minute", asset, { limit: 20 })
+                .then((resp) => {
+                    resolve(resp[asset][0].c);
+                })
+                .catch((err) => {
+                    writeToEventLog(err);
+                });
+        });
+        return currPrice;
     }
-
 
     // Get current price of stock we hold
     async get_asset_curr(asset, portfolio) {
-        let price = 0
+        let price = 0;
         // Print the quantity of shares for each position.
         portfolio.forEach(function (position) {
             if (position.symbol === asset) {
                 price = position.current_price;
             }
         });
-        return price
+        return price;
     }
-
 
     // Get qty we want to order
     async get_qty(price, qty, val, target) {
-        val *= target
-        let newQty = val / price
-        newQty += 0.2
-        newQty = parseInt(newQty)
-        console.log(`Our new target quantity is ${newQty}`)
-        return (newQty - qty)
+        val *= target;
+        let newQty = val / price;
+        newQty += 0.2;
+        newQty = parseInt(newQty);
+        // writeToEventLog(`Our new target quantity is ${newQty}`);
+        return newQty - qty;
     }
-
 
     // Placing our orders
     async order_target(asset, target, qty) {
-        console.log(`Let's get ${target} of our portfolio holding ${asset}`)
         if (target !== 0 && qty !== 0) {
-            let side = 'buy'
-            if (qty < 0) side = 'sell'
+            let side = "buy";
+            if (qty < 0) side = "sell";
             qty = Math.abs(qty);
-            console.log(`Trying to ${side} ${qty} shares of ${asset}`)
-            await this.submitMarketOrder(asset, qty, side)
-        } else console.log(`Sufficiently leveraged in ${asset}`)
-        console.log("\n===================")
+            // writeToEventLog(`Trying to ${side} ${qty} shares of ${asset}`);
+            await this.submitMarketOrder(asset, qty, side);
+        } else writeToEventLog(`Sufficiently leveraged in ${asset}`);
+        writeToEventLog(`Let's get ${target} of our portfolio holding ${asset}`);
     }
-
 
     // Submit a market order if quantity is above 0.
     async submitMarketOrder(stock, quantity, side) {
-        let errors = false
+        let errors = false;
         if (quantity > 0) {
-            let resp = await this.alpaca.createOrder({
-                symbol: stock,
-                qty: quantity,
-                side: side,
-                type: "market",
-                time_in_force: "day",
-            }).catch((err) => {
-                console.log(
-                    "Order of |" +
-                    quantity +
-                    " " +
-                    stock +
-                    " " +
-                    side +
-                    "| did not go through."
+            let resp = await this.alpaca
+                .createOrder({
+                    symbol: stock,
+                    qty: quantity,
+                    side: side,
+                    type: "market",
+                    time_in_force: "day",
+                })
+                .catch((err) => {
+                    writeToEventLog(
+                        "Order of |" +
+                        quantity +
+                        " " +
+                        stock +
+                        " " +
+                        side +
+                        "| did not go through."
                     );
-                    errors = true
-                    console.log(err.message)
-            });
-            if ( !errors ){
-                console.log(
-                    "Market order of |" +
-                    quantity +
-                    " " +
-                    stock +
-                    " " +
-                    side +
-                    "| completed."
-                    );
+                    errors = true;
+                    writeToEventLog(err.message);
+                });
+            if (!errors) {
+                this.updateChart()
             }
         } else {
-        console.log(
-            "Quantity is <=0, order of |" +
-            quantity +
-            " " +
-            stock +
-            " " +
-            side +
-            "| not sent."
+            writeToEventLog(
+                "Quantity is <=0, order of |" +
+                quantity +
+                " " +
+                stock +
+                " " +
+                side +
+                "| not sent."
             );
         }
     }
+
+
+    updateChart() {
+        this.alpaca.getAccount().then((resp) => {
+            this.chart.data.datasets[0].data.push({
+                t: new Date(),
+                y: resp.equity,
+            });
+            this.chart.update();
+        });
+        this.updatePositions();
+        this.updateOrders();
+    }
+
+
+    updateOrders() {
+        $("#orders-log").empty();
+        this.alpaca
+            .getOrders({
+                status: "open",
+            })
+            .then((resp) => {
+                resp.forEach((order) => {
+                    $("#orders-log").prepend(
+                        `<div class="order-inst">
+                <p class="order-fragment">${order.symbol}</p>
+                <p class="order-fragment">${order.qty}</p>
+                <p class="order-fragment">${order.side}</p>
+                <p class="order-fragment">${order.type}</p>
+            </div>`
+                    );
+                });
+            });
+    }
+
+
+    // Log our positions
+    async updatePositions() {
+        $("#positions-log").empty();
+        this.alpaca.getPositions().then((resp) => {
+            resp.forEach((position) => {
+                $("#positions-log").prepend(
+                    `<div class="position-inst">
+                <p class="position-fragment">${position.symbol}</p>
+                <p class="position-fragment">${position.qty}</p>
+                <p class="position-fragment">${position.side}</p>
+                <p class="position-fragment">${position.unrealized_pl}</p>
+            </div>`
+                );
+            });
+        });
+    }
+} // End of algo
+
+
+// Run the algo class.
+function runScript() {
+    var API_KEY = $("#api-key").val();
+    var API_SECRET = $("#api-secret").val();
+    var F1 = new algo(API_KEY, API_SECRET);
+    F1.run();
 }
-
-
-// Run the Falcon One class.
-var F1 = new FalconOne(API_KEY, API_SECRET, PAPER);
-F1.run();
+function killScript() {
+    $("#event-log").html("Killing script.");
+    F1.kill();
+}
+function writeToEventLog(text) {
+    $("#event-log").prepend(`<p class="event-fragment">${text}</p>`);
+}
